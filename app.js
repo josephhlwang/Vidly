e8a99cc8f3e6bb8ca0b697d13ad15e6637dce869
Joseph Wang <joseph@Josephs-MacBook-Pro.local>
2020-09-04 15:49:40 -0400

    Fixed port

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => logger.info(`Listening on port ${port}...`));
